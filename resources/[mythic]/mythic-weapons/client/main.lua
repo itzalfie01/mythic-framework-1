1762d6dc5ab6fcd53c9b536f08b6e087baedf1ba
itzalfie01 <77110102+itzalfie01@users.noreply.github.com>
2025-04-15 20:21:28 +0100

    Remove pistol whip

--- resources/[mythic]/mythic-weapons/client/main.lua
+++ resources/[mythic]/mythic-weapons/client/main.lua
@@ -6,3 +6,16 @@ CreateThread(function()
 		SetWeaponDamageModifier(k, v + 0.0)
 	end
 end)
+
+CreateThread(function()
+    while true do
+        DisableControlAction(1, 351, true)
+        SetPedCanPlayAmbientAnims(PlayerPedId(), false)
+        if IsPedArmed(PlayerPedId(), 6) then
+            DisableControlAction(1, 140, true)
+            DisableControlAction(1, 141, true)
+            DisableControlAction(1, 142, true)
+        end
+        Wait(5)
+    end
+end)
